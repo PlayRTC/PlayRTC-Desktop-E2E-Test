1f0424a99e84ce17d94a2c542be80312dd47b9c5
dididi <dfdgsdfg@gmail.com>
2015-04-17 18:09:42 +0900

    update test case

--- README.md
+++ README.md
@@ -8,20 +8,24 @@
 
 ## 테스트 수행
 
-1) [Node.js](http://nodejs.org) 설치
+- [Node.js](http://nodejs.org) 설치
 ```sh
 $ git clone https://github.com/playrtc/playrtc-desktop-e2e-test
 $ cd playrtc-desktop-e2e-test
 $ npm install
 ```
 
-2) [Selenium 저장소에서](http://selenium-release.storage.googleapis.com/index.html) `selenium-server-standalone-x.x.x.jar`를 다운로드 받아 `ㅠㅑㅜ` 폴더에 넣고 `nightwatch.json`에서 Selenium 파일명 지정
+- [Selenium 저장소](http://selenium-release.storage.googleapis.com/index.html)에서 `selenium-server-standalone-x.x.x.jar`를 다운로드 받아 `bin` 폴더에 넣고 `nightwatch.json`에서 Selenium 파일명 지정
 
-3) [Chrome Driver 저장소](http://chromedriver.storage.googleapis.com/index.html)에서 `chromedriver` 다운로드 받고 `bin` 폴더에 넣기
+- [Chrome Driver 저장소](http://chromedriver.storage.googleapis.com/index.html)에서 `chromedriver` 다운로드 받고 `bin` 폴더에 넣기
 
-4) 2개의 크롬 피어를 통해 테스트
+- 2개의 크롬 피어를 통해 테스트
 ```sh
 $ ./bin/nightwatch.js --env chrome,chrome
+```
+
+```dos
+c:\> node ./bin/nightwatch.js --env chrome,chrome
 ```
 
 COPYRIGHT (c) 2015 SK TELECOM CO. LTD. ALL RIGHTS RESERVED.
--- tests/connect.js
+++ tests/connect.js
@@ -1,34 +1,34 @@
-module.exports = new (function() {
+module.exports = new(function() {
   var firstClient = process.env.__NIGHTWATCH_ENV_KEY == 'chrome_1';
   var testCases = this;
 
-  testCases['opening the browser and navigating to the url'] = function (client) {
+  testCases['opening the browser and navigating to the url'] = function(client) {
     client
       .url('http://playrtc.github.io/PlayRTC-Javascript-Code-Samples/basis/p2p.html')
       .waitForElementVisible('body', 1000);
   };
 
   if (firstClient) {
-    testCases['wait for clients to become connected'] = function(client) {
+    testCases['wait for clients to become connected 1'] = function(client) {
       client
         .waitForElementVisible('#callerLocalVideo', 1500)
         .click('#createChannel')
+        .pause(5000)
         .waitForClientConnected('#callerLocalVideo', 5000)
-        .waitForClientConnected('#callerRemoteVideo', 8000,
-          'Remote video stream (%s) was connected in %s ms.');
+        .getValue('#createChannelId', function(result) {
+          client.setValue('#connectChannelId', result.value);
+        });
     };
 
-    testCases['wait for peer to disconnect'] = function (client) {
+    testCases['wait for clients to become connected 2'] = function(client) {
       client
-        .pause(1000)
-        .getValue('#createChannelId', function(result) {
-          client.setValue('#connectChannelId', result);
-        })
+        .waitForElementVisible('#callerRemoteVideo', 1500)
         .click('#connectChannel')
-        .waitForElementNotPresent('#calleeLocalVideo', 10000);
+        .pause(5000)
+        .waitForClientConnected('#callerRemoteVideo', 10000);
     };
   } else {
-    testCases.suspend = function (client) {
+    testCases.suspend = function(client) {
       client.pause(10000);
     };
   }
